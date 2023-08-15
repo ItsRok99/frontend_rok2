472db2a101e453d77cb645ec2d91dfa25e8642a4
ItsRok99 <rok.salemovic@student.um.si>
2023-08-15 15:25:59 +0200

    xyz3

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,9 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		host: '0.0.0.0',  // To bind to all available network interfaces.
+		port: 7000,       // Adjust accordingly if you want a different port.
+	}
 });
